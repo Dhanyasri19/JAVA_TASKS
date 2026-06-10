0a77538a5b0383d72c7ccd570558cb20ddbc55a4
Dhanyasri19 <dhanyasri694@gmail.com>
2026-06-10 13:36:12 +0530

    Added files

---
+++ QueueLinkedList.java
@@ -0,0 +1,27 @@
+import java.util.*;
+    public class QueueLinkedList {
+        public static void main(String[] args) {
+            Queue<Integer> queue = new LinkedList<>();
+
+            // Enqueue operation
+            queue.add(10);
+            queue.add(20);
+            queue.add(30);
+            System.out.println("Queue after enqueuing: " + queue);
+
+            // Dequeue operation
+            int removedElement = queue.poll();
+            System.out.println("Dequeued element: " + removedElement);
+            System.out.println("Queue after dequeuing: " + queue);
+
+            // Peek operation
+            int frontElement = queue.peek();
+            System.out.println("Front element: " + frontElement);
+
+            // Check if queue is empty
+            boolean isEmpty = queue.isEmpty();
+            System.out.println("Is the queue empty? " + isEmpty);
+        }
+
+
+}
